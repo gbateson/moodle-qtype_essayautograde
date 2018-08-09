539cafc120249de0e4bfcc815acb97ae2bea2a6d
Gordon Bateson <gordonbateson@gmail.com>
2018-08-09 15:39:28 +0900

    qtype/essayautograde scroll to appropriate section when adding more grade bands or target phrases

--- version.php
+++ version.php
@@ -29,8 +29,8 @@ $plugin->cron      = 0;
 $plugin->component = 'qtype_essayautograde';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015111600; // Moodle 3.0
-$plugin->version   = 2018080950;
-$plugin->release   = '2018-08-09 (50)';
+$plugin->version   = 2018080951;
+$plugin->release   = '2018-08-09 (51)';
 
 // https://docs.moodle.org/dev/Releases
 // Moodle 3.5 2018051700 17 May 2018
