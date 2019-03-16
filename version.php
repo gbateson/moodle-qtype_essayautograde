0a5624fc53acf807eabebacd92c0cd36694212fe
Gordon Bateson <gordonbateson@gmail.com>
2019-03-16 00:39:47 -0400

    Well spotted and thanks for the fix German!

--- version.php
+++ version.php
@@ -30,8 +30,8 @@ $plugin->cron      = 0;
 $plugin->component = 'qtype_essayautograde';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015111600; // Moodle 3.0
-$plugin->version   = 2019031573;
-$plugin->release   = '2019-03-15 (73)';
+$plugin->version   = 2019031674;
+$plugin->release   = '2019-03-16 (74)';
 
 // https://docs.moodle.org/dev/Releases
 // Moodle 3.6 2018120300  3 Dec 2018
