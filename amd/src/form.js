d39f590d0c4d315a94a30fb26e8b71bf914890c3
Gordon Bateson <gordonbateson@gmail.com>
2021-07-14 17:01:27 +0900

    qtype_essayautograde add Javascript to show/hide editors on settings page depending on setting of responseformat

--- amd/src/form.js
+++ amd/src/form.js
@@ -34,9 +34,124 @@ define(["jquery"], function($) {
      * initialize this AMD module
      */
     FORM.init = function() {
+        FORM.init_responseformat();
         FORM.init_target_phrases();
         FORM.init_add_button("addbands", "id_gradebands");
         FORM.init_add_button("addphrases", "id_targetphrases");
+    };
+
+    /**
+     * If the responseformat is changed, for example to "Plain text",
+     * then change the format of the related editors too.
+     */
+    FORM.init_responseformat = function() {
+        var fmt = document.querySelector("select[name=responseformat]");
+        $(fmt).change(function(){
+            var fmtvalue = this.options[this.selectedIndex].value;
+            var fmttext = this.options[this.selectedIndex].innerText;
+            var fmtnumber = 0;  // Moodle format
+            if (fmtvalue == "plain" || fmtvalue == "monospaced") {
+                fmtnumber = 2; // Plain text format
+            } else if (fmtvalue == "editor" || fmtvalue == "editorfilepicker") {
+                fmtnumber = 1; // HTML format
+            }
+            var names = new Array("responsetemplate", "responsesample");
+            names.forEach(function(name){
+                // Changing the format may not immediately change the display in the browser.
+                // For example, changing from "plain" to "editor" will not magically load an editor.
+                // However, the new value will be returned to the server,
+                // and used next time the question is displayed or edited.
+                var fmt = document.querySelector("[name='" + name + "[format]']");
+                if (fmt) {
+                    if (fmt.matches("select")) {
+                        // the user has selected "Plain text editor" in the Editor preferences
+                        for (var i=0; i < fmt.options.length; i++) {
+                            if (fmt.options[i].value == fmtnumber) {
+                                fmt.options[i].selected = true;
+                            }
+                        }
+                    } else if (fmt.matches("input[type=hidden]")) {
+                        fmt.value = fmtnumber;
+                    }
+                }
+                var txt = document.querySelector("[name='" + name + "[text]']");
+                if (txt) {
+                    var editor = null;
+                    var editable = null;
+
+                    var elm = txt.previousElementSibling;
+                    if (elm && elm.matches(".editor_atto")) {
+                        editor = elm;
+                        editable = editor.querySelector("#id_" + name + "editable");
+                    }
+
+                    var elm = txt.nextElementSibling;
+                    if (elm && elm.matches("#id_" + name + "_parent")) {
+                        editor = elm;
+                        elm = editor.querySelector("#id_" + name + "_ifr");
+                        if (elm) {
+                            // cross browser access to <iframe> <body>
+                            elm = (elm.contentWindow || elm.contentDocument);
+                            if (elm.document) {
+                                elm = elm.document;
+                            }
+                            editable = elm.body;
+                        }
+                    }
+
+                    if (editor) {
+                        if (fmtnumber == 0 || fmtnumber == 1) {
+                            // hide TEXTAREA
+                            txt.hidden = true;
+                            txt.style.display = "none";
+                        } else {
+                            // show TEXTAREA
+                            txt.hidden = false;
+                            txt.style.display = "";
+                            // Transfer editor content to txt.
+                            // Note that this will strip HTML tags.
+                            if (editable) {
+                                txt.value = editable.innerText;
+                            }
+                        }
+                        if (fmtnumber == 0 || fmtnumber == 2) {
+                            // hide editor
+                            editor.hidden = true;
+                            editor.style.display = "none";
+                            elm = editor.parentNode.querySelector(".editor_atto_notification");
+                            if (elm) {
+                                elm.hidden = true;
+                                elm.style.display = "none";
+                            }
+                        } else {
+                            // show editor
+                            editor.hidden = false;
+                            editor.style.display = "";
+                            // Transfer txt content to editor.
+                            if (editable) {
+                                editable.innerHTML = txt.value;
+                            }
+                        }
+                        var msg = txt.parentNode.querySelector("#noinline-message");
+                        if (fmtnumber == 0) {
+                            // show message to explain why TEXTAREA and editor have disappeared
+                            if (msg) {
+                                msg.hidden = false;
+                                msg.style.display = "";
+                            } else {
+                                msg = document.createElement("DIV");
+                                msg.setAttribute("id", "noinline-message");
+                                msg.appendChild(document.createTextNode(fmttext));
+                                txt.parentNode.appendChild(msg);
+                            }
+                        } else if (msg) {
+                            msg.hidden = true;
+                            msg.style.display = "none";
+                        }
+                    }
+                }
+            });
+        });
     };
 
     /**
--- version.php
+++ version.php
@@ -30,8 +30,8 @@ $plugin->cron      = 0;
 $plugin->component = 'qtype_essayautograde';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015111600; // Moodle 3.0
-$plugin->version   = 2021071406;
-$plugin->release   = '2021-07-14 (06)';
+$plugin->version   = 2021071407;
+$plugin->release   = '2021-07-14 (07)';
 
 // https://docs.moodle.org/dev/Releases
 // Moodle 3.11 2021051700 17 May 2021
